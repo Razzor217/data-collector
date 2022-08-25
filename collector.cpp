b14c7b4155edb1dabb4a7aafe18697d3c11f18bc
Max Beddies <max.beddies@t-online.de>
2022-08-25 12:35:04 +0000

    add comments and references

--- collector.cpp
+++ collector.cpp
@@ -26,9 +26,22 @@ void Collector::monitor_and_collect()
     std::cout << "Start monitoring " << input_path << std::endl;
     std::cout << "Please type <q> and press <RETURN> to stop the program and quit." << std::endl;
 
+    // start both worker threads
     monitor_thread = std::thread{&Collector::monitor, this};
     collector_thread = std::thread{&Collector::collect, this};
 
+
+    /*
+     * The polling mechanism is heavily inspired by the inotify example that is
+     * part of the inotify man pages.
+     * The poll man page was also referenced.
+     *
+     * See https://man7.org/linux/man-pages/man7/inotify.7.html
+     * and https://man7.org/linux/man-pages/man2/poll.2.html
+     */
+
+
+    // start polling stdin for program termination, i.e. input of <q>
     pollfd poll_descriptor {};
     poll_descriptor.fd = STDIN_FILENO;
     poll_descriptor.events = POLLIN;
@@ -37,8 +50,10 @@ void Collector::monitor_and_collect()
 
     for (;;)
     {
+        // poll from stdin
         int poll_number {poll(&poll_descriptor, 1, -1)};
 
+        // error or interrupt
         if (poll_number < 0)
         {
             if (errno == EINTR)
@@ -49,12 +64,13 @@ void Collector::monitor_and_collect()
             break;
         }
 
+        // event arrived
         if (poll_number > 0)
         {
             if (poll_descriptor.revents & POLLIN)
             {
                 /*
-                 * read from stdin until newline character is found,
+                 * read from stdin until 'q' character is found,
                  * then quit out of loop
                  */
                 while (read(STDIN_FILENO, &buffer, 1) > 0 && buffer != 'q')
@@ -78,6 +94,18 @@ void Collector::monitor_and_collect()
 
 void Collector::monitor()
 {
+    /*
+     * The usage of inotify is heavily inspired by the example that is part of
+     * the inotify man pages.
+     * Man pages of specific inotify methods were also referenced.
+     *
+     * See https://man7.org/linux/man-pages/man7/inotify.7.html,
+     *     https://man7.org/linux/man-pages/man2/inotify_init.2.html,
+     *     https://man7.org/linux/man-pages/man2/inotify_add_watch.2.html,
+     * and https://man7.org/linux/man-pages/man2/read.2.html
+     */
+
+    // initialize inotify
     int file_descriptor {inotify_init1(IN_NONBLOCK)};
 
     if (file_descriptor < 0)
@@ -88,6 +116,7 @@ void Collector::monitor()
         return;
     }
 
+    // add the input directory to inotify's watch list
     int watch_descriptor {inotify_add_watch(file_descriptor, input_path.c_str(), IN_CREATE)};
 
     if (watch_descriptor < 0)
@@ -98,14 +127,26 @@ void Collector::monitor()
         return;
     }
 
+    /*
+     * The polling mechanism is heavily inspired by the inotify example that is
+     * part of the inotify man pages.
+     * The poll man page was also referenced.
+     *
+     * See https://man7.org/linux/man-pages/man7/inotify.7.html
+     * and https://man7.org/linux/man-pages/man2/poll.2.html
+     */
+
     pollfd poll_descriptor {};
     poll_descriptor.fd = file_descriptor;
     poll_descriptor.events = POLLIN;
 
+    // repeat until interrupt signal by main thread is sent
     while (is_running.load())
     {
+        // poll from inotify
         int poll_number {poll(&poll_descriptor, 1, 30)};
 
+        // error or interrupt
         if (poll_number < 0)
         {
             if (errno == EINTR)
@@ -116,10 +157,12 @@ void Collector::monitor()
             break;
         }
 
+        // event arrived
         if (poll_number > 0)
         {
             if (poll_descriptor.revents & POLLIN)
             {
+                // handle event separately
                 handle_file_event(file_descriptor);
             }
         }
@@ -127,6 +170,7 @@ void Collector::monitor()
 
     std::cout << "Stop monitoring " << input_path << std::endl;
 
+    // remove input directory from watch list and close inotify file descriptor
     inotify_rm_watch(file_descriptor, watch_descriptor);
     close(file_descriptor);
 
@@ -136,9 +180,11 @@ void Collector::monitor()
 
 void Collector::collect()
 {
+    // repeat until interrupt signal by main thread is sent
     while (is_running.load())
     {
-        if (!queue->empty())
+        // if file creation events are available, handle all of them
+        while (!queue->empty())
         {
             std::filesystem::path const file {queue->pop()};
 
@@ -150,6 +196,7 @@ void Collector::collect()
 
             collect_disk_usage(file_names, temporaries, file.parent_path());
 
+            // create a unique archive name by hashing the name of the created file
             std::string hash {std::to_string(std::hash<std::string>{}(std::string{file.filename().c_str()}))};
             store_files
             (
@@ -186,6 +233,21 @@ void Collector::set_regex(std::regex const& regex)
 }
 
 
+/*
+ * For std::filesystem, the cppreference was referenced.
+ *
+ * See https://en.cppreference.com/w/cpp/filesystem,
+ *     https://en.cppreference.com/w/cpp/filesystem/path,
+ *     https://en.cppreference.com/w/cpp/filesystem/directory_iterator,
+ *     https://en.cppreference.com/w/cpp/filesystem/create_directory,
+ *     https://en.cppreference.com/w/cpp/filesystem/remove.
+ *
+ * For the execution of bash commands, cppreference was referenced.
+ *
+ * See https://en.cppreference.com/w/cpp/filesystem/remove.
+ */
+
+
 std::vector<std::filesystem::path> Collector::collect_files
 (
     std::filesystem::path const& path,
@@ -198,6 +260,7 @@ std::vector<std::filesystem::path> Collector::collect_files
     {
         case FileSelection::FILES:
         {
+            // iterate through directory and select regular files only
             std::vector<std::filesystem::path> files;
             for (auto const& entry : std::filesystem::directory_iterator{path})
             {
@@ -211,6 +274,7 @@ std::vector<std::filesystem::path> Collector::collect_files
         case FileSelection::FILES_AND_DIRECTORIES:
         default:
         {
+            // traverse the complete directory tree, i.e. recurse into subdirectories
             std::vector<std::filesystem::path> files;
             for (auto const& entry : std::filesystem::recursive_directory_iterator{path})
             {
@@ -235,6 +299,7 @@ void Collector::collect_disk_usage
 
     for (auto const& file : files)
     {
+        // append disk usage information to text file
         std::system(std::string {"du -sh " + file.native() + " >> " + usage.native()}.c_str());
     }
 
@@ -255,6 +320,7 @@ void Collector::store_files
 
     std::string command {"tar -cf " + output_file.native()};
 
+    // add every given file to command, creating a file list
     for (auto const& file : files)
     {
         command += " " + file.native();
@@ -277,8 +343,10 @@ void Collector::handle_file_event(int const file_descriptor)
 {
     alignas(inotify_event) char buffer[BUFFER_SIZE];
 
+    // repeat until interrupt signal by main thread is sent
     while (is_running.load())
     {
+        // read file creation events into buffer
         int n = read(file_descriptor, buffer, BUFFER_SIZE);
 
         if (n < 0 && errno != EAGAIN)
@@ -289,17 +357,20 @@ void Collector::handle_file_event(int const file_descriptor)
             return;
         }
 
+        // no file creation events available, break out of loop
         if (n <= 0)
         {
             break;
         }
 
         inotify_event const* event {};
+        // handle every file creation event in the buffer
         for (char* ptr {buffer}; ptr < buffer + n; ptr += sizeof(inotify_event) + event->len)
         {
             event = (inotify_event const*) ptr;
             if (event->len)
             {
+                // if the file name matches the regex, push the file to the queue
                 if ((event->mask & IN_CREATE) && std::regex_match(event->name, file_regex))
                 {
                     std::cout << "New matching file/directory '" << event->name << "' created" << std::endl;
